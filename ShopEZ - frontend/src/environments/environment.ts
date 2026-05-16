1161a4c555d9479a21507819386cc09cae802324
y-jayasree <151768482+y-jayasree@users.noreply.github.com>
2026-05-16 13:03:52 +0530

    Add files via upload

---
+++ ShopEZ - frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiGatewayUrl: 'https://localhost:7000',
+  apiBaseUrl: 'https://localhost:7002',  
+  apiUrl: 'https://localhost:7000/api'  
+};
+
+
+// export const environment = {
+//   production: false,
+//   apiGatewayUrl: 'http://localhost:7000',
+//   apiBaseUrl: 'http://localhost:7002',
+//   apiUrl: 'http://localhost:7000/api'
+// };
